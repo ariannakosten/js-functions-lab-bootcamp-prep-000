fc2e2ed23fe1fbf2d200b0424cd54a1ee61857cb
Arianna Kosten <arianna.kosten@gmail.com>
2019-04-09 03:51:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ function happyHolidays() {
 return ("Happy holidays!");
 }
 
+var holiday = "Easter"
 var name = "you"
 function happyHolidaysTo(name) {
   return (`Happy holidays, ${name}!`);
 }
 
-var holiday = "Easter",
 function happyHolidayTo(holiday, name) {
   return (`Happy ${holiday}, ${name}!`);
 }
